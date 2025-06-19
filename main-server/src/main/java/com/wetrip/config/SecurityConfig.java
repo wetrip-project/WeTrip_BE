e645ef0db8bf7a04eb1d17e24a484c0c772ef4da
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2025-06-19 19:48:12 +0900

    CORS 설정 추가  (#3)
    * feat: add CORS properties for dynamic injection
    
    * feat: add CORS configuration on Spring Web, Spring Security

--- main-server/src/main/java/com/wetrip/config/SecurityConfig.java
+++ main-server/src/main/java/com/wetrip/config/SecurityConfig.java
@@ -1,24 +1,37 @@
 package com.wetrip.config;
 
+import com.wetrip.dto.common.CorsProperties;
 import com.wetrip.service.user.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties({CorsProperties.class})
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsProperties corsProperties;
 
     // 생성자 주입으로 받음 → @Service로 등록된 CustomOAuth2UserService 자동 주입
     private final CustomOAuth2UserService customOAuth2UserService; // 사용자 정의 OAuth2 사용자 정보 서비스 (ex: 카카오 로그인 처리 로직)
     private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler; // 로그인 성공 시 추가 동작을 정의하는 핸들러
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 기능 비활성화 (개발 중 혹은 API 서버일 경우 비활성화함)
                 .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // H2 콘솔 확인용
                 .authorizeHttpRequests(auth -> auth // URL 경로별 접근 권한 설정
@@ -35,5 +48,19 @@ public class SecurityConfig {
                 );
 
         return http.build(); // 최종적으로 보안 필터 체인을 반환
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList(corsProperties.origins()));
+        config.setAllowedMethods(Arrays.asList(corsProperties.methods()));
+        config.setMaxAge(corsProperties.maxAge());
+        config.setAllowCredentials(true);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
